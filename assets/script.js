db286d26e6ebb5a58103575c9b8c431b9dd412da
Davina Crahet <davina.crahet@gmail.com>
2023-11-26 20:01:13 +0100

    Ajout flèches

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,47 @@
+// Création d'un tableau contenant les 4 images de notre carrousel
 const slides = [
   {
-    image: "slide1.jpg",
+    image: "../assets/images/slideshow/slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
   {
-    image: "slide2.jpg",
+    image: "../assets/images/slideshow/slide2.jpg",
     tagLine:
       "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
   },
   {
-    image: "slide3.jpg",
+    image: "../assets/images/slideshow/slide3.jpg",
     tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
   },
   {
-    image: "slide4.png",
+    image: "../assets/images/slideshow/slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
 
-// Variables et constantes
-const dots = document.querySelectorAll(".dots");
+// Variables et Constantes
+const dots = document.querySelector(".dots");
 const arrowRight = document.querySelector(".container-banner .arrow_right");
-const arrowLeft = document.querySelector("#banner .arrow_left");
-const img = document.querySelector("#banner img");
-const titreImage = document.querySelector("# p");
+const arrowLeft = document.querySelector(".container-banner .arrow_left");
+const img = document.querySelector(".container-banner img");
+const titreImage = document.querySelector(".container-banner p");
 let index = 0;
+
+// Fonction création et affichage des points
+function bulletPoints() {
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    dots.appendChild(dot);
+
+    if (i == index) {
+      dot.classList.add("dot_selected");
+    }
+  }
+}
+bulletPoints();
+
+const allDots = document.querySelectorAll(".dots .dot");
 
 // Fonction au clic sur flèche droite
 function clickArrowRight() {
@@ -42,7 +59,6 @@ function clickArrowRight() {
 
 // Fonction au clic sur flèche gauche
 function clickArrowLeft() {
-  console.log(index);
   if (index > 0) {
     allDots[index].classList.remove("dot_selected");
     index--;
@@ -50,8 +66,13 @@ function clickArrowLeft() {
     allDots[index].classList.remove("dot_selected");
     index = 3;
   }
-  console.log(index);
   img.src = slides[index].image;
   titreImage.innerHTML = slides[index].tagLine;
   allDots[index].classList.add("dot_selected");
 }
+
+arrowRight.onclick = clickArrowRight;
+arrowLeft.onclick = clickArrowLeft;
+
+// arrowRight.addEventListener("click", clickArrowRight);
+// arrowLeft.addEventListener("click", clickArrowLeft);
